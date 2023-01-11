9f8a14679bf461db8972fb724b470b6ae5faacf6
loctt12345 <trnthinlc@gmail.com>
2023-01-11 15:58:51 +0700

    update style

--- Resource/DemoCustomerScreen/js/qrcode.js
+++ Resource/DemoCustomerScreen/js/qrcode.js
@@ -1,6 +1,6 @@
 function onScanSuccess(qrCodeMessage) {
     window.location.href =
-        "http://127.0.0.1:5500/DemoCustomerScreen/"
+        "http://127.0.0.1:5500/Resource/DemoCustomerScreen/"
         + qrCodeMessage + ".html";
 }
 
