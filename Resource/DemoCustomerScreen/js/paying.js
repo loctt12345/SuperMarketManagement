b1be5e2c522d1ace9d25c246aab2ce056b8bf501
loctt12345 <trnthinlc@gmail.com>
2023-01-14 17:59:42 +0700

    add paying page

---
+++ Resource/DemoCustomerScreen/js/paying.js
@@ -0,0 +1,9 @@
+function openWalletInfo() {
+    let walletInfo = document.getElementById("wallet_info");
+    walletInfo.style.display = "flex";
+}
+
+function closeWalletInfo() {
+    let walletInfo = document.getElementById("wallet_info");
+    walletInfo.style.display = "none";
+}
