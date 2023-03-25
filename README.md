8c1967c3151399f7a5e461c6c1cf046505a16144
khanhlh1706 <lohungkhanh1495@gmail.com>
2023-03-26 00:03:51 +0700

    add pagination

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# SuperMarketManagement
-Web application for SWP391 project using spring boot framework.
+## Supermarket Management with Barcode Scanner
+#### Description:
+Project Supermarket Scanner is a sales management system for supermarkets, using barcode scanner as the mainstream.The system allows product management, order management and employee management.
+
+#### Request
+Spring Boot 2.7.2 or later.\
+Microsoft SQL Server Management 2019 or later.\
+GitBash or GitHub is installed.\
+NetBean 12.3 or later.
+
+#### Setting
+Clone repository: git clone https://github.com/loctt12345/SuperMarketManagement.git \
+Open NetBean and open folder cloned.\
+Install dependencies: Shift + F11.\
+Access to AppApplication.java\
+Start the server: Shift + F6.
+
+#### Use
+##### Product Management
+Manage product list: add, edit, delete, search products.
+##### Administrator management
+Manage employee list: add, edit, delete, search employees.\
+Manage product list: add, edit, delete, search products.\
+Manage QRcode list: save QRcode order from storage employees and shippers.\
+Manage Order list: view order history, view order detail.
+##### Feature for customer
+Scan barcode for view product detail.\
+Manage cart: add, edit, delete.\
+Payment with PayPal.
+##### Feature for storage employee
+QRCode scanner for packaging product.
+##### Feature for shipper 
+QRCode scanner for delivery.
+##### Contribute
+If you want to contribute to the project, you can follow these steps:
+Fork repository.
+Clone repository from your account: git clone https://github.com/loctt12345/SuperMarketManagement.git \
+Create a new branch: git checkout -b feature \
+Make changes and commit: git commit -m "Add feature" \
+Push changes to your branch: git push origin feature \
+Create a pull request and describe your changes.
+##### Author
+Name: Tran Thien Loc\
+Email: locttse160275@fpt.edu.vn
+
+Thanks to the contributors who contributed to this project, including:\
+Lo Hung Khanh.\
+Huynh Nhat Thien Hoang.\
+Nguyen Chanh Nhat Minh.\
+Phan Nhat Tan.
--- app/src/main/resources/static/js/order_management.js
+++ app/src/main/resources/static/js/order_management.js
@@ -6,32 +6,56 @@
 
 
 
+var totalPage = parseInt(document.getElementById('numOrders').value);
 
+var currentPage = parseInt(document.getElementById('pageNum').value);
+$(document).ready(function () {
+    console.log("ready!");
 
-window.onload = function ()
-{
-    var size = parseInt(document.getElementById('size').value);
+    window.onload = function ()
+    {
+        var size = parseInt(document.getElementById('size').value);
 
-    for (var i = 0; i < size; ++i) {
-        var x = document.getElementById('id' + i).value;
-        let qrcode = new QRCode(document.getElementById("orderid" + i),
-                {
-                    text: x,
-                    width: 100,
-                    height: 100,
-                    colorDark: "#000000",
-                    colorLight: "#ffffff",
-                    correctLevel: QRCode.CorrectLevel.H
-                });
-        document.getElementById('select' + i).value 
-                = document.getElementById('status' + i).value;
+        for (var i = 0; i < size; ++i) {
+            var x = document.getElementById('id' + i).value;
+            let qrcode = new QRCode(document.getElementById("orderid" + i),
+                    {
+                        text: x,
+                        width: 100,
+                        height: 100,
+                        colorDark: "#000000",
+                        colorLight: "#ffffff",
+                        correctLevel: QRCode.CorrectLevel.H
+                    });
+            document.getElementById('select' + i).value
+                    = document.getElementById('status' + i).value;
+        }
+
+        let totals = document.getElementsByClassName('total');
+        for (var i = 0; i < totals.length; ++i) {
+            var total = parseFloat(totals[i].innerHTML);
+            totals[i].innerHTML = total.toLocaleString('de-DE',
+                    {style: 'currency', currency: 'VND'});
+        }
+        document.getElementsByClassName('page-item')[0].className = 'page-item';
+        document.getElementsByClassName('page-item')[currentPage - 1].className = 'page-item active';
     }
+});
+var pagingCount = 0;
+$(function () {
+    window.pagObj = $('#pagination').twbsPagination({
+        totalPages: totalPage,
+        visiblePages: 10,
+        onPageClick: function (event, page) {
+            console.log(event);
+            if (pagingCount !== 0) {
+                location.href =
+                        "/admin/order_management?page="
+                        + page;
 
-    let totals = document.getElementsByClassName('total');
-    for (var i = 0; i < totals.length; ++i) {
-        var total = parseFloat(totals[i].innerHTML);
-        totals[i].innerHTML = total.toLocaleString('de-DE',
-                {style: 'currency', currency: 'VND'});
-    }
-
-};
+            } else {
+            }
+            pagingCount++;
+        }
+    });
+});
